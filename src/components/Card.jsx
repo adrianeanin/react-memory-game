5e94965890986fd8b1017a4114d123408d1fb19f
adriananin <113365066+adriananin@users.noreply.github.com>
2023-10-30 15:38:16 +0000

    refactor to improve readability

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,10 +6,4 @@ const Card = ({ emojiData, onClick }) => {
   );
 };
 
-// console.log(isClicked);
-
-// clicked ? console.log("you lose") : setScore((prev) => prev + 1);
-// console.log(score);
-// setBest(score);
-
 export default Card;
